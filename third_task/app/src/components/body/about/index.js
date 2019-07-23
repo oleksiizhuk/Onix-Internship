d853116ab2e43a8fc9c17933fc35d79e48436979
OleksiiZhuk <oleksiizhuk.att@gmail.com>
2019-07-23 13:21:49 +0300

    add destruct object in about

--- third_task/app/src/components/body/about/index.js
+++ third_task/app/src/components/body/about/index.js
@@ -4,19 +4,21 @@ import MyPhoto from '../../../images/my-photo.jpg';
 
 const about = (props) => {
 
-    const name = props.date.person[0].name;
-    const surname = props.date.person[0].surname;
-    const age = props.date.person[0].age;
-    const city = props.date.person[0].city;
-    const interests = props.date.person[0].interests;
-    const hobby = props.date.person[0].hobby;
-    const job = props.date.person[0].job;
-    const fb = props.date.person[0].social[0].fb;
-    const twitter = props.date.person[0].social[0].twitter;
-    const gMail = props.date.person[0].social[0].gMail;
-    const github = props.date.person[0].social[0].github;
-    const linkedIn = props.date.person[0].social[0].linkedIn;
+    /*  const name = props.date.person[0].name;
+      const surname = props.date.person[0].surname;
+      const age = props.date.person[0].age;
+      const city = props.date.person[0].city;
+      const interests = props.date.person[0].interests;
+      const hobby = props.date.person[0].hobby;
+      const job = props.date.person[0].job;
+      const fb = props.date.person[0].social[0].fb;
+      const twitter = props.date.person[0].social[0].twitter;
+      const gMail = props.date.person[0].social[0].gMail;
+      const github = props.date.person[0].social[0].github;
+      const linkedIn = props.date.person[0].social[0].linkedIn;*/
 
+    let {name, surname, age, city, interests, hobby, job,
+        social: {fb, twitter, gMail, github, linkedIn} = {}} = props.date.person;
     return (
         <div className="section-2" id="section-2">
             <div className="container">
@@ -48,7 +50,7 @@ const about = (props) => {
 
                         <div className="about__info__social">
                             <div className="about__info__social__item">
-                                <a href= {fb}>
+                                <a href={fb}>
                                     <i className="fab fa-facebook-f"/>
                                 </a>
                             </div>
--- third_task/app/src/components/body/index.js
+++ third_task/app/src/components/body/index.js
@@ -7,28 +7,48 @@ export default class body extends Component {
     constructor(props) {
         super(props);
 
+        /* this.state = {
+             person: [
+                 {
+                     id: 1,
+                     name: 'oleksii',
+                     surname: 'zhuk',
+                     age: '26',
+                     city: 'krop',
+                     interests: "computer science",
+                     hobby: "TV show, travel",
+                     job: 'frontend web developer',
+                     social: [
+                         {
+                             fb: 'https://www.facebook.com/profile.php?id=100007163145347',
+                             twitter: 'https://twitter.com/Oleksii82814989',
+                             gMail: 'oleksiizhuk.att@gmail.com',
+                             github: 'https://github.com/oleksiizhuk',
+                             linkedIn: 'https://www.linkedin.com/in/%D0%B0%D0%BB%D0%B5%D0%BA%D1%81%D0%B5%D0%B9-%D0%B6%D1%83%D0%BA-317a92162/'
+                         }
+                     ]
+                 }
+             ]
+         };*/
+
         this.state = {
-            person: [
-                {
-                    id: 1,
-                    name: 'oleksii',
-                    surname: 'zhuk',
-                    age: '26',
-                    city: 'krop',
-                    interests: "computer science",
-                    hobby: "TV show, travel",
-                    job: 'frontend web developer',
-                    social: [
-                        {
-                            fb: 'https://www.facebook.com/profile.php?id=100007163145347',
-                            twitter: 'https://twitter.com/Oleksii82814989',
-                            gMail: 'oleksiizhuk.att@gmail.com',
-                            github: 'https://github.com/oleksiizhuk',
-                            linkedIn: 'https://www.linkedin.com/in/%D0%B0%D0%BB%D0%B5%D0%BA%D1%81%D0%B5%D0%B9-%D0%B6%D1%83%D0%BA-317a92162/'
-                        }
-                    ]
+            person: {
+                id: 1,
+                name: 'oleksii',
+                surname: 'zhuk',
+                age: '26',
+                city: 'krop',
+                interests: "computer science",
+                hobby: "TV show, travel",
+                job: 'frontend web developer',
+                social: {
+                    fb: 'https://www.facebook.com/profile.php?id=100007163145347',
+                    twitter: 'https://twitter.com/Oleksii82814989',
+                    gMail: 'oleksiizhuk.att@gmail.com',
+                    github: 'https://github.com/oleksiizhuk',
+                    linkedIn: 'https://www.linkedin.com/in/%D0%B0%D0%BB%D0%B5%D0%BA%D1%81%D0%B5%D0%B9-%D0%B6%D1%83%D0%BA-317a92162/'
                 }
-            ]
+            }
         };
 
     }
